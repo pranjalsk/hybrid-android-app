4dd17a242c94b976d84b6fefc20a398af995ffae
pranjalsk <pkaranka@asu.edu>
2017-11-25 11:19:48 -0700

    init

---
+++ app/src/main/java/edu/asu/ser421/lab6/hybridapp/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.asu.ser421.lab6.hybridapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
