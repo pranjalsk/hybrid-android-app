7d1cc7fdf896fd68abb89b955b81f6b1685c13a2
pranjalsk <pkaranka@asu.edu>
2017-11-26 11:28:54 -0700

    cities dropdonw

--- app/src/main/java/edu/asu/ser421/lab6/hybridapp/CityList.java
+++ app/src/main/java/edu/asu/ser421/lab6/hybridapp/CityList.java
@@ -6,12 +6,13 @@ import android.view.View;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 import android.util.Log;
 import java.util.ArrayList;
 
-public class CityList extends Activity {
+public class CityList extends Activity implements View.OnClickListener{
     private static final String tag="mytag";
     String[] cities = new String[] {
             "Denver",
@@ -27,6 +28,8 @@ public class CityList extends Activity {
     };
     ArrayList<String> citiesChecked =  new ArrayList<String>();
 
+    private JsHandler _jsHandler;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -37,7 +40,7 @@ public class CityList extends Activity {
         ListView listView = ( ListView ) findViewById(R.id.listview);
 
         // Instantiating an array adapter for listview
-        ArrayAdapter< String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, cities);
+        ArrayAdapter<String> adapter  = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_multiple_choice, cities);
         listView.setAdapter(adapter);
 
         //Defining an item click listener
@@ -60,5 +63,25 @@ public class CityList extends Activity {
 
         // Setting the ItemClickEvent listener for the listview
         listView.setOnItemClickListener(itemClickListener);
+
+
+        //------------------------------------------------------
+        // GetWeather Button
+        Button btn = (Button)findViewById(R.id.getWeatherButton);
+        btn.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.getWeatherButton:
+                _jsHandler.javaFnCall(citiesChecked.get(0),citiesChecked.get(1),citiesChecked.get(2),citiesChecked.get(3),citiesChecked.get(4));
+                break;
+
+            default:
+                break;
+        }
     }
 }
